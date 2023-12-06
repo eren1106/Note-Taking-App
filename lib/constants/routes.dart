22764ea66f885a9a8f990a5e86063387b3b98e86
eren <erenkuek1106@gmail.com>
2023-12-07 06:54:29 +0800

    done setup go router with bottom nav bar

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -2,4 +2,6 @@ class ROUTES_NAME {
   static const noteCollections = 'noteCollections';
   static const notes = 'notes';
   static const noteDetail = 'noteDetail';
+  static const stickyNotes = 'stickyNotes';
+  static const settings = 'settings';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,9 @@ import 'package:note_taking_app/view/screens/home_screen.dart';
 import 'package:note_taking_app/view/screens/note_collections_screen.dart';
 import 'package:note_taking_app/view/screens/note_detail_screen.dart';
 import 'package:note_taking_app/view/screens/notes_screen.dart';
+import 'package:note_taking_app/view/screens/settings_screen.dart';
+import 'package:note_taking_app/view/screens/sticky_notes_screen.dart';
+import 'package:note_taking_app/view/widgets/scaffold_with_nested_navigation.dart';
 import 'package:note_taking_app/view_model/note_view_model.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
@@ -15,33 +18,127 @@ void main() async {
   runApp(const MyApp());
 }
 
+// private navigators
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorAKey = GlobalKey<NavigatorState>(debugLabel: 'shellA');
+final _shellNavigatorBKey = GlobalKey<NavigatorState>(debugLabel: 'shellB');
+final _shellNavigatorCKey = GlobalKey<NavigatorState>(debugLabel: 'shellC');
+final _shellNavigatorDKey = GlobalKey<NavigatorState>(debugLabel: 'shellD');
+
 /// The route configuration.
 final GoRouter _router = GoRouter(
+  initialLocation: '/',
+  navigatorKey: _rootNavigatorKey,
   routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const HomeScreen();
+    // GoRoute(
+    //   path: '/',
+    //   builder: (BuildContext context, GoRouterState state) {
+    //     return const HomeScreen();
+    //   },
+    //   routes: <RouteBase>[
+    //     GoRoute(
+    //       path: ROUTES_NAME.noteCollections,
+    //       builder: (BuildContext context, GoRouterState state) {
+    //         return const NoteCollectionsScreen();
+    //       },
+    //     ),
+    //     GoRoute(
+    //       path: ROUTES_NAME.notes,
+    //       builder: (BuildContext context, GoRouterState state) {
+    //         return const NotesScreen();
+    //       },
+    //     ),
+    //     GoRoute(
+    //       path: '${ROUTES_NAME.noteDetail}/:id',
+    //       builder: (BuildContext context, GoRouterState state) {
+    //         final id = state.pathParameters['id']!;
+    //         return NoteDetailScreen(id: id);
+    //       },
+    //     ),
+    //   ],
+    // ),
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        // the UI shell
+        return ScaffoldWithNestedNavigation(navigationShell: navigationShell);
       },
-      routes: <RouteBase>[
-        GoRoute(
-          path: ROUTES_NAME.noteCollections,
-          builder: (BuildContext context, GoRouterState state) {
-            return const NoteCollectionsScreen();
-          },
+      branches: [
+        // first branch
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorAKey,
+          routes: [
+            // top route inside branch
+            GoRoute(
+              path: '/',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: HomeScreen(),
+              ),
+              // routes: [
+              //   // child route
+              //   GoRoute(
+              //     path: ROUTES_NAME.noteCollections,
+              //     builder: (context, state) => const NoteCollectionsScreen(),
+              //   ),
+              // ],
+            ),
+          ],
         ),
-        GoRoute(
-          path: ROUTES_NAME.notes,
-          builder: (BuildContext context, GoRouterState state) {
-            return const NotesScreen();
-          },
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorBKey,
+          routes: [
+            // top route inside branch
+            GoRoute(
+              path: '/${ROUTES_NAME.noteCollections}',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: NoteCollectionsScreen(),
+              ),
+              // routes: [
+              //   // child route
+              //   GoRoute(
+              //     path: ROUTES_NAME.noteCollections,
+              //     builder: (context, state) => const NoteCollectionsScreen(),
+              //   ),
+              // ],
+            ),
+          ],
         ),
-        GoRoute(
-          path: '${ROUTES_NAME.noteDetail}/:id',
-          builder: (BuildContext context, GoRouterState state) {
-            final id = state.pathParameters['id']!;
-            return NoteDetailScreen(id: id);
-          },
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorCKey,
+          routes: [
+            // top route inside branch
+            GoRoute(
+              path: '/${ROUTES_NAME.stickyNotes}',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: StickyNotesScreen(),
+              ),
+              // routes: [
+              //   // child route
+              //   GoRoute(
+              //     path: ROUTES_NAME.noteCollections,
+              //     builder: (context, state) => const NoteCollectionsScreen(),
+              //   ),
+              // ],
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorDKey,
+          routes: [
+            // top route inside branch
+            GoRoute(
+              path: '/${ROUTES_NAME.settings}',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: SettingsScreen(),
+              ),
+              // routes: [
+              //   // child route
+              //   GoRoute(
+              //     path: ROUTES_NAME.noteCollections,
+              //     builder: (context, state) => const NoteCollectionsScreen(),
+              //   ),
+              // ],
+            ),
+          ],
         ),
       ],
     ),
@@ -73,30 +170,10 @@ class _MyAppState extends State<MyApp> {
       providers: [
         ChangeNotifierProvider.value(value: NoteViewModel()),
       ],
-      child: MaterialApp(
+      child: MaterialApp.router(
         title: kAppName,
         theme: kThemeData,
-        home: Scaffold(
-          body: _screenOptions.elementAt(_selectedIndex),
-          bottomNavigationBar: BottomNavigationBar(
-            items: kNavItems
-                .map(
-                  (navItem) => BottomNavigationBarItem(
-                    icon: Icon(navItem.iconData),
-                    label: navItem.label,
-                  ),
-                )
-                .toList(),
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.purple,
-            unselectedItemColor: Colors.grey,
-            onTap: _onItemTapped,
-          ),
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const HomeScreen(),
-        },
+        routerConfig: _router,
       ),
     );
   }
